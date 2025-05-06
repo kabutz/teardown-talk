35470c83194d934ab6cf65ad769a1c24f4545460
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-05-06 20:00:02 +0200

    demos

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
@@ -1,10 +1,46 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.*;
+
 public class LockSplittingDemo {
     public static void main(String... args) throws InterruptedException {
         // TODO: submit 10m Integers to a BlockingQueue, with either SPSC or SPMC
         //  Use LinkedBlockingQueue, LinkedBlockingDeque and ArrayBlockingQueue
 
         // TODO: Show how the two locks work in enqueue and dequeue
+        // var queue = new ArrayBlockingQueue<Integer>(10_000);
+        // var queue = new LinkedBlockingDeque<Integer>();
+        var queue = new LinkedBlockingQueue<Integer>();
+        for (int i = 0; i < 10; i++) {
+            test(queue);
+        }
+    }
+
+    private static void test(BlockingQueue<Integer> queue) {
+        System.out.println(queue.getClass().getName());
+        long time = System.nanoTime();
+        try {
+            try (var pool = Executors.newCachedThreadPool()) {
+                pool.submit(() -> {
+                    for (int i = 0; i < 10_000_000; i++) {
+                        queue.put(42);
+                    }
+                    return "done";
+                });
+                int numberOfConsumers = 2;
+                for (int c = 0; c < numberOfConsumers; c++) {
+                    pool.submit(() -> {
+                        for (int i = 0; i < 10_000_000 / numberOfConsumers; i++) {
+                            queue.take();
+                        }
+                        return "done";
+                    });
+                }
+            }
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+        System.gc();
     }
 }
--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
@@ -1,10 +1,80 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.atomic.*;
+import java.util.stream.*;
+
 public class Striped64Demo {
+    private static class Counter {
+        private volatile long value;
+        public void increment() {
+            value++;
+        }
+    }
     public static void main(String... args) {
         // TODO: Compare time taken to increment 100m times in parallel
         //  using AtomicLong and LongAdder
 
         // TODO: Quick look at how Striped64 works
+
+        for (int i = 0; i < 10; i++) {
+            testSequential();
+            // testParallel();
+        }
+    }
+
+    private static void testParallel() {
+        long adderTime = System.nanoTime();
+        try {
+            var adder = new LongAdder();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(_ -> adder.increment());
+        } finally {
+            adderTime = System.nanoTime() - adderTime;
+            System.out.printf("adderTime = %dms%n", (adderTime / 1_000_000));
+        }
+
+        long atomicTime = System.nanoTime();
+        try {
+            var atomic = new AtomicLong();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(_ -> atomic.incrementAndGet());
+        } finally {
+            atomicTime = System.nanoTime() - atomicTime;
+            System.out.printf("atomicTime = %dms%n", (atomicTime / 1_000_000));
+        }
+    }
+    private static void testSequential() {
+        long adderTime = System.nanoTime();
+        try {
+            var adder = new LongAdder();
+            for (int i = 0; i < 100_000_000; i++) {
+                adder.increment();
+            }
+        } finally {
+            adderTime = System.nanoTime() - adderTime;
+            System.out.printf("adderTime = %dms%n", (adderTime / 1_000_000));
+        }
+
+        long atomicTime = System.nanoTime();
+        try {
+            var atomic = new AtomicLong();
+            for (int i = 0; i < 100_000_000; i++) {
+                atomic.incrementAndGet();
+            }
+        } finally {
+            atomicTime = System.nanoTime() - atomicTime;
+            System.out.printf("atomicTime = %dms%n", (atomicTime / 1_000_000));
+        }
+        long counterTime = System.nanoTime();
+        try {
+            var counter = new Counter();
+            for (int i = 0; i < 100_000_000; i++) {
+                counter.increment();
+            }
+        } finally {
+            counterTime = System.nanoTime() - counterTime;
+            System.out.printf("counterTime = %dms%n", (counterTime / 1_000_000));
+        }
+
     }
 }
