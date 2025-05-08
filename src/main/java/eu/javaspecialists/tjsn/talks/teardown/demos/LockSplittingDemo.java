fc6f79e026edfd373283d6e80ba0406d6f0f34f5
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-05-08 17:33:19 +0100

    dmoes

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
@@ -1,10 +1,65 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import eu.javaspecialists.tjsn.talks.teardown.util.*;
+
+import java.util.concurrent.*;
+
 public class LockSplittingDemo {
+
+    protected static final int ELEMENTS = 10_000_000;
+
     public static void main(String... args) throws InterruptedException {
         // TODO: submit 10m Integers to a BlockingQueue, with either SPSC or SPMC
         //  Use LinkedBlockingQueue, LinkedBlockingDeque and ArrayBlockingQueue
 
         // TODO: Show how the two locks work in enqueue and dequeue
+
+        for (int i = 0; i < 10; i++) {
+            // test(new ArrayBlockingQueue<>(100_000));
+            // test(new LinkedBlockingDeque<>());
+            test(new LinkedBlockingQueue<>());
+        }
+    }
+
+    private static void test(BlockingQueue<Integer> q) {
+        long time = System.nanoTime();
+        try {
+            System.out.println(q.getClass().getSimpleName());
+            try (var executor = Executors.newCachedThreadPool()) {
+                executor.submit(() -> {
+                    var bm = new Benchmark();
+                    bm.start();
+                    for (int i = 0; i < ELEMENTS; i++) {
+                        try {
+                            q.put(42);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    bm.stop();
+                    System.out.println("producer: " + bm);
+                });
+                int numberOfConsumers = 2;
+                for (int j = 0; j < numberOfConsumers; j++) {
+                    executor.submit(() -> {
+                        var bm = new Benchmark();
+                        bm.start();
+                        for (int i = 0; i < ELEMENTS/numberOfConsumers; i++) {
+                            try {
+                                q.take();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        bm.stop();
+                        System.out.println("consumer: " + bm);
+                    });
+                }
+            }
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+        System.gc();
     }
 }
--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
@@ -1,10 +1,47 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.atomic.*;
+import java.util.stream.*;
+
 public class Striped64Demo {
     public static void main(String... args) {
         // TODO: Compare time taken to increment 100m times in parallel
         //  using AtomicLong and LongAdder
 
+        long noContTime = System.nanoTime();
+        try {
+            // var myadder = new AtomicLong();
+            var myadder = new LongAdder();
+            for (int i = 0; i < 100_000_000; i++) {
+                myadder.increment();
+                // myadder.incrementAndGet();
+            }
+            System.out.println(myadder.longValue());
+        } finally {
+            noContTime = System.nanoTime() - noContTime;
+            System.out.printf("noContTime = %dms%n", (noContTime / 1_000_000));
+        }
+
+        long time = System.nanoTime();
+        try {
+            var atomic = new AtomicLong();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(i -> atomic.incrementAndGet());
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+
+        long adderTime = System.nanoTime();
+        try {
+            var adder = new LongAdder();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(i -> adder.increment());
+            System.out.println(adder.longValue());
+        } finally {
+            adderTime = System.nanoTime() - adderTime;
+            System.out.printf("adderTime = %dms%n", (adderTime / 1_000_000));
+        }
         // TODO: Quick look at how Striped64 works
     }
 }
