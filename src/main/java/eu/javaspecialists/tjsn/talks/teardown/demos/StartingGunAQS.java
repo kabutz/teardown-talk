8843d6f5e8ae1fd375fe7f75e3c77c9620073ce7
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-05-14 11:41:39 +0300

    jPrime Demos

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/StartingGunAQS.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/StartingGunAQS.java
@@ -3,14 +3,43 @@ package eu.javaspecialists.tjsn.talks.teardown.demos;
 
 import eu.javaspecialists.tjsn.talks.teardown.startinggun.*;
 
+import java.util.concurrent.locks.*;
+
 public class StartingGunAQS implements StartingGun {
     // TODO: Copy implementation from CountDownLatch
 
     public void awaitUninterruptibly() {
-        throw new UnsupportedOperationException("todo");
+        sync.acquireShared(1);
     }
 
     public void ready() {
-        throw new UnsupportedOperationException("todo");
+        sync.releaseShared(1);
     }
+
+    private final Sync sync = new Sync();
+
+    private static final class Sync extends AbstractQueuedSynchronizer {
+        private static final long serialVersionUID = 4982264981922014374L;
+
+        Sync() {
+            setState(1);
+        }
+
+        protected int tryAcquireShared(int acquires) {
+            return (getState() == 0) ? 1 : -1;
+        }
+
+        protected boolean tryReleaseShared(int releases) {
+            // Decrement count; signal when transition to zero
+            for (;;) {
+                int c = getState();
+                if (c == 0)
+                    return false;
+                int nextc = c - 1;
+                if (compareAndSetState(c, nextc))
+                    return nextc == 0;
+            }
+        }
+    }
+
 }
