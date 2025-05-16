608c5c0ddf2d2bbb48ab815556c37efa407b7177
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-05-16 06:53:43 +0200

    demos

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
@@ -1,10 +1,50 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.*;
+
 public class LockSplittingDemo {
     public static void main(String... args) throws InterruptedException {
         // TODO: submit 10m Integers to a BlockingQueue, with either SPSC or SPMC
         //  Use LinkedBlockingQueue, LinkedBlockingDeque and ArrayBlockingQueue
 
         // TODO: Show how the two locks work in enqueue and dequeue
+
+        for (int i = 0; i < 10; i++) {
+            // test(new ArrayBlockingQueue<>(10_000), 2);
+            // test(new LinkedBlockingDeque<>(), 2);
+            test(new LinkedBlockingQueue<>(), 2);
+        }
+    }
+
+    private static void test(BlockingQueue<Integer> queue, int numberOfConsumers) {
+        long time = System.nanoTime();
+        try {
+            try (var pool = Executors.newCachedThreadPool()) {
+                pool.submit(() -> {
+                    for (int i = 0; i < 10_000_000; i++) {
+                        try {
+                            queue.put(42);
+                        } catch (InterruptedException e) {
+                            throw new CancellationException("interrupted");
+                        }
+                    }
+                });
+                for (int c = 0; c < numberOfConsumers; c++) {
+                    pool.submit(() -> {
+                        for (int i = 0; i < 10_000_000 / numberOfConsumers; i++) {
+                            try {
+                                queue.take();
+                            } catch (InterruptedException e) {
+                                throw new CancellationException("interrupted");
+                            }
+                        }
+                    });
+                }
+            }
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+        System.gc();
     }
 }
