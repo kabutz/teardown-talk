9b8a50f88fab5b212c92379ad98cee2e70a0d9c8
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-06-19 19:35:29 +0200

    demos

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/LockSplittingDemo.java
@@ -1,10 +1,53 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.*;
+
 public class LockSplittingDemo {
     public static void main(String... args) throws InterruptedException {
         // TODO: submit 10m Integers to a BlockingQueue, with either SPSC or SPMC
         //  Use LinkedBlockingQueue, LinkedBlockingDeque and ArrayBlockingQueue
 
+        for (int i = 0; i < 10; i++) {
+            // test(new ArrayBlockingQueue<>(10_000), 2);
+            test(new LinkedBlockingDeque<>(), 2);
+            // test(new LinkedBlockingQueue<>(), 1);
+        }
         // TODO: Show how the two locks work in enqueue and dequeue
+    }
+
+    private static void test(BlockingQueue<Integer> queue, int consumerCount) {
+        System.out.println(queue.getClass().getSimpleName());
+        long time = System.nanoTime();
+        try {
+            try (var pool = Executors.newCachedThreadPool()) {
+                pool.submit(() -> {
+                    for (int i = 0; i < 10_000_000; i++) {
+                        try {
+                            queue.put(42);
+                        } catch (InterruptedException e) {
+                            throw new CancellationException("interrupted");
+                        }
+                    }
+                });
+                for (int c = 0; c < consumerCount; c++) {
+                    pool.submit(() -> {
+                        for (int i = 0; i < 10_000_000 / consumerCount; i++) {
+                            try {
+                                queue.take();
+                            } catch (InterruptedException e) {
+                                throw new CancellationException("interrupted");
+                            }
+                        }
+                    });
+                }
+            }
+        } finally {
+            time = System.nanoTime() - time;
+            System.out.printf("time = %dms%n", (time / 1_000_000));
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.gc();
+        }
     }
 }
--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
@@ -1,9 +1,31 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.atomic.*;
+import java.util.stream.*;
+
 public class Striped64Demo {
     public static void main(String... args) {
         // TODO: Compare time taken to increment 100m times in parallel
         //  using AtomicLong and LongAdder
+        long atomicTime = System.nanoTime();
+        try {
+            var atomic = new AtomicInteger();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(i -> atomic.incrementAndGet());
+        } finally {
+            atomicTime = System.nanoTime() - atomicTime;
+            System.out.printf("atomicTime = %dms%n", (atomicTime / 1_000_000));
+        }
+
+        long adderTime = System.nanoTime();
+        try {
+            var adder = new LongAdder();
+            IntStream.range(0, 100_000_000).parallel()
+                    .forEach(i -> adder.increment());
+        } finally {
+            adderTime = System.nanoTime() - adderTime;
+            System.out.printf("adderTime = %dms%n", (adderTime / 1_000_000));
+        }
 
         // TODO: Quick look at how Striped64 works
     }
