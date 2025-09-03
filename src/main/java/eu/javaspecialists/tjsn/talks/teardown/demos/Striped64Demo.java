83c690ba97327c97f43fa31aec8b142c6b5a3318
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2025-09-03 13:59:39 +0200

    Ready for JavaZone Talk

--- src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
+++ src/main/java/eu/javaspecialists/tjsn/talks/teardown/demos/Striped64Demo.java
@@ -1,10 +1,21 @@
 package eu.javaspecialists.tjsn.talks.teardown.demos;
 
+import java.util.concurrent.atomic.*;
+import java.util.stream.*;
+
 public class Striped64Demo {
     public static void main(String... args) {
         // TODO: Compare time taken to increment 100m times in parallel
         //  using AtomicLong and LongAdder
+        var atomicLong = new AtomicLong();
+        IntStream.range(0, 100_000_000)
+                .parallel()
+                .forEach(_ -> atomicLong.getAndIncrement());
 
         // TODO: Quick look at how Striped64 works
+        var longAdder = new LongAdder();
+        IntStream.range(0, 100_000_000)
+                .parallel()
+                .forEach(_ -> longAdder.increment());
     }
 }
